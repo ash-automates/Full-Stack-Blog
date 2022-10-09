3236464559d36417dbac7ab677f78ff104e616f1
shachem <sekki.hachem@outlook.com>
2022-10-09 21:03:49 +0100

    Reset project to start working on blog app

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 const http = require("http");
-const lodash = require("lodash");
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.write("Hello World");
-  res.end();
-});
+const server = http.createServer((req, res) => {});
 
 server.listen(8000, () => {
   console.log("listening on port 8000");
