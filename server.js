564ba100d201921252116fda8457f935bb50ab40
shachem <sekki.hachem@outlook.com>
2022-10-09 22:49:44 +0100

    Add 2 extra routes for the web application

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  let path = "./views";
+  switch (req.url) {
+    case "/":
+      path += "/index.html";
+      break;
+    case "/about":
+      path += "/about.html";
+      break;
+    default:
+      path += "/404.html";
+      break;
+  }
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", (error, data) => {
+  fs.readFile(path, (error, data) => {
     if (error) {
       console.log(error);
       res.end();
