f39c6fc8308ba85f3f0a896451fbf3feaffbebd8
shachem <sekki.hachem@outlook.com>
2022-10-09 21:14:14 +0100

    Respond to requests from '/' with index.html

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  fs.readFile("./views/index.html", (error, data) => {
+    if (error) {
+      console.log(error);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
 
 server.listen(8000, () => {
   console.log("listening on port 8000");
