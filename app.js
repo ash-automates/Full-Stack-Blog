c84c98b60c5f081349947d903ce0839d464592d8
shachem <sekki.hachem@outlook.com>
2022-10-10 19:25:53 +0100

    Redirect from deprecated "/about-us" to "/about"

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
 });
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
