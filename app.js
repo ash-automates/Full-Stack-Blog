1cabc1c9093ae21bad9c5ec18800d0d98322cd14
shachem <sekki.hachem@outlook.com>
2022-10-10 14:49:20 +0100

    Send HTML files instead for 2 different routes

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const app = express();
 app.listen(8000);
 
 app.get("/", (req, res) => {
-  res.send("<p>Hello World!</p>");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
 });
