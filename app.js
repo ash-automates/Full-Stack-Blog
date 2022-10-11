3bcf5d75a96ad72ee1f33a3ddb5fe0fa1f3d90c3
shachem <sekki.hachem@outlook.com>
2022-10-11 18:31:02 +0100

    implement "ejs" view engine to render webpages

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
 app.listen(8000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
-});
-
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+  res.render("index");
 });
