0597f585af855e7ed24c0df464e6e769f2f8c8a7
shachem <sekki.hachem@outlook.com>
2022-10-12 05:28:48 +0100

    Link rest of the routes to their respective views

--- app.js
+++ app.js
@@ -21,3 +21,15 @@ app.get("/", (req, res) => {
   ];
   res.render("index", { title: "Home", blogs: data });
 });
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
