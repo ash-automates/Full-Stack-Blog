e62d83851aa35ab125c17214766e11228536d3a0
shachem <sekki.hachem@outlook.com>
2022-10-13 17:59:47 +0100

    Create sandbox routes to practice "Mongoose" ODM

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 require("dotenv").config();
 
 // settings
@@ -10,9 +11,45 @@ app.use(express.static("public"));
 app.use(morgan("dev"));
 
 // mongoDB
-const connection = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.4p3upij.mongodb.net/?retryWrites=true&w=majority`;
+const connection = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.4p3upij.mongodb.net/${process.env.ATLAS_DB}?retryWrites=true&w=majority`;
 mongoose.connect(connection).then((result) => {
   app.listen(8000);
+});
+
+app.get("/create-blog", (req, res) => {
+  const blog = new Blog({
+    title: "My blog 4",
+    snippet: "Test Snippet",
+    body: "Test Body",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/get-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get("/get-blog", (req, res) => {
+  Blog.findById("6347d5dee31b978c1455243b")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.get("/", (req, res) => {
